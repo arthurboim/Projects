2f27b7e183ec4fa97b9688a4ffd101897d29fe12
arthu <arthu@DESKTOP-OQI8DDA>
2019-02-28 22:19:27 -0800

    Update index to start and end print the amount of files

--- Downloader/src/DownloaderMain.java
+++ Downloader/src/DownloaderMain.java
@@ -3,13 +3,14 @@ import java.io.IOException;
 public class DownloaderMain {
 	public static void main(String[] args) throws IOException 
 	{			
-		
-		for (int i = 2227092;i<2228592;i++)  
-		{					  
+		int start= 2228592;
+		int end = 2229176;
+		for (;start<end;start++)  
+		{	
+			System.out.println("Files amount: "+(end - start));
 			//downloader.DownloadFile("https://listen.freshrecords.ru/?id="+i,"F:\\Downloads\\"+i+".mp3");
-			Thread t = new Thread(new Download("https://listen.freshrecords.ru/?id="+i,"C:\\Downloads\\"+i+".mp3"));
-			while(java.lang.Thread.activeCount()>101);
-			//46468
+			Thread t = new Thread(new Download("https://listen.freshrecords.ru/?id="+start,"C:\\Downloads\\"+start+".mp3"));
+			while(java.lang.Thread.activeCount()>100);
 			t.start();
 		}
 
